8fe6347545571558a4803642cb48ad9508a969ae
Marius Biegger <biegger.marius@gmail.com>
2024-07-28 18:47:15 +0200

    hmm

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.argv.includes('dev') ? '/web-aeon' : process.env.BASE_PATH
+			
         },
 
 	}
