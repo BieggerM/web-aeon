c442008efffc9c0841587c234b17dc1267ccd1d3
Marius Biegger <biegger.marius@gmail.com>
2024-08-05 21:33:07 +0200

    big update on looks

--- src/lib/Presentation.svelte
+++ src/lib/Presentation.svelte
@@ -9,47 +9,40 @@
     export let imageSrc = "images/aeondesktop.png"; // Default path
     export let imageAlt = "Gnome Desktop";
     export let imageCaption = "Aeon Desktop Powered by Gnome";
-
     // Exported variables for content
     export let heading = "Gnome Desktop";
-    export let text = "For a user-friendly and intuitive desktop experience, Aeon leverages the power of GNOME. GNOME is a popular and widely-used desktop environment known for its clean aesthetics, intuitive layout, and focus on user productivity. With GNOME, you'll enjoy a clutter-free workspace.";
+    export let text = "For a user-friendly and intuitive desktop experience, Aeon leverages the power of GNOME.GNOME is a popular and widely-used desktop environment known for its clean aesthetics, intuitive layout, and focus on user productivity. With GNOME, you'll enjoy a clutter-free workspace.";
 </script>
 
-<div class="grid grid-cols-1 md:grid-cols-2 gap-8 items-center text-center space-y-5 my-40">
-    <div class="mx-2 justify-center items-center">
+    <div class="mx-2 mt-10">
         <div
 				class="wrapper"
-				use:inview={{ unobserveOnEnter: true, rootMargin: '0%' }}
+				use:inview={{ unobserveOnEnter: false, rootMargin: '20%' }}
 				on:change={({ detail }) => {
 					isInView = detail.inView;
 				}}
 			>
             {#if isInView}
+            <div class="">
                 <img
-                    class="rounded-2xl h-auto"
+                    class="rounded-2xl h-full w-full"
                     src={imageSrc}
                     alt={imageAlt}
                     in:fly={{ delay: 0, duration: 700, x: -100, opacity: 0.5, easing: quintOut }}
                 />
-               
+            </div>
             {/if}
+            {#if !isInView}
+            <div class="opacity-0">
+                <img
+                    class="rounded-2xl h-full w-full"
+                    src={imageSrc}
+                    alt={imageAlt}
+                    in:fly={{ delay: 0, duration: 700, x: -100, opacity: 0.5, easing: quintOut }}
+                />
+            </div>
+           
+            {/if}
+           
         </div>
-    </div>
-
-    <div
-				class="wrapper"
-				use:inview={{ unobserveOnEnter: true, rootMargin: '0%' }}
-				on:change={({ detail }) => {
-					isInView = detail.inView;
-				}}
-			>
-        {#if isInView}
-            <article class="mx-5">
-                <h1 class="text-3xl" in:fade={{ delay: 800, duration: 500 }}>{heading}</h1>
-                <p class="text-2xl text-left mt-5" in:fade={{ delay: 1200, duration: 500 }}>
-                    {text}
-                </p>
-            </article>
-        {/if}
-    </div>
-</div>
+    </div>   
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import GlanceCard from '../lib/GlanceCard.svelte';
 	import Presentation from '$lib/Presentation.svelte';
 
-
 	let isInView: boolean;
 	let flatpackInView: boolean;
 </script>
@@ -10,7 +9,7 @@
 <div class="container h-screen mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
 		<div class="">
-			<h2 class="h2">
+			<h2 class="h2 text-5xl">
 				Welcome to
 				<p class="font-bold underline underline-offset-3 decoration-4 decoration-primary-500">
 					Aeon.
@@ -25,7 +24,7 @@
 		</figure>
 		<!-- / -->
 		<div class="flex justify-center space-x-2 mx-2">
-			<p class="text-xl">The linux desktop for people who want to "get stuff done".</p>
+			<p class="text-2xl">The linux desktop for people who want to "get stuff done".</p>
 		</div>
 		<div class="flex justify-center space-x-4 mt-4">
 			<button
@@ -102,14 +101,13 @@
 <div class="container mx-auto justify-center mt-14">
 	<div class="space-y-10 text-left flex flex-col items-center">
 		<article class="text-wrap mx-5">
-			<h2 class="h2">
+			<h2 class="text-5xl">
 				<span
 					class="bg-gradient-to-br from-primary-500 to-secondary-400 bg-clip-text text-transparent box-decoration-clone"
 					>Based On Trusted Tooling</span
 				>
 			</h2>
-
-			<p class="text-xl mt-5">
+			<p class="text-2xl mt-5">
 				Aeon is great for anyone who wants a computer that <p1
 					class="font-semibold underline decoration-primary-500">"just works"</p1
 				>; in particular it is perfect for the "lazy developer" who prefers to spend their time
@@ -121,48 +119,144 @@
 </div>
 <div class="container mx-auto mt-16">
 	<div class="space-y-5 text-left flex flex-col">
-		<Presentation
-			imageSrc="images/aeondesktop.png"
-			imageAlt="Gnome Desktop"
-			imageCaption="Aeon Desktop Powered by Gnome"
-			heading="Gnome Desktop"
-			text="For a user-friendly and intuitive desktop experience, Aeon leverages the power of GNOME. GNOME is a popular and widely-used desktop environment known for its clean aesthetics, intuitive layout, and focus on user productivity. With GNOME, you'll enjoy a clutter-free workspace."
-		/>
-		<Presentation
-			imageSrc="images/flat.png"
-			imageAlt="Flatpak"
-			imageCaption="Flatpak Applications inside"
-			heading="Flatpak Applications"
-			text="Aeon provides a streamlined approach to application management through its
-    integration with Flatpak. Flatpak is a modern technology that simplifies the
-    installation and updates of applications. Unlike traditional methods, Flatpak
-    packages applications as self-contained units, including all their necessary
-    dependencies. This offers several benefits for Aeon users."
-		/>
+		<div class="grid grid-cols-1 md:grid-cols-2 gap-8 items-center text-cente space-y-5">
+			<Presentation
+				imageSrc="images/aeondesktop.png"
+				imageAlt="Gnome Desktop"
+				imageCaption="Aeon Desktop Powered by Gnome"
+				heading="Gnome Desktop"
+				text=" "
+			/>
+			<article class="mx-5 md:p-32">
+				<h1 class="text-5xl text-primary-500">Gnome Desktop</h1>
+				<h1 class="text-3xl text-primary-500 mt-3">is elegantly practical</h1>
+				<p class="text-2xl text-left mt-6">
+					For a user-friendly and intuitive desktop experience, Aeon leverages the power of GNOME.
+				</p>
+				<p class="text-2xl text-left mt-6">
+					GNOME stands as a beacon of user-friendly design in the Linux ecosystem. Its intuitive
+					interface and focus on simplicity make it a welcoming environment for both newcomers and
+					seasoned users alike.
+				</p>
+				<p class="text-2xl text-left mt-6">
+					GNOME's commitment to regular updates ensures a steady stream of enhancements and
+					features, keeping the desktop experience fresh and modern. This dedication to
+					accessibility and innovation aligns seamlessly with the evolving needs of the Linux
+					community, making GNOME a compelling choice for those seeking a polished and user-centric
+					desktop environment.
+				</p>
+			</article>
+		</div>
+		<div class="grid grid-cols-1 md:grid-cols-2 gap-8 items-center text-cente space-y-5">
+			<Presentation
+				imageSrc="images/flat.png"
+				imageAlt="Flatpak"
+				imageCaption="Flatpak Applications inside"
+				heading="Flatpak Applications"
+				text="Aeon provides a streamlined approach to application management through its
+integration with Flatpak. Flatpak is a modern technology that simplifies the
+installation and updates of applications. Unlike traditional methods, Flatpak
+packages applications as self-contained units, including all their necessary
+dependencies. This offers several benefits for Aeon users."
+			/>
+			<article class="mx-5 md:p-32">
+				<h1 class="text-5xl text-primary-500">Flatpak Applications</h1>
+				<h1 class="text-3xl text-primary-500 mt-3">as first class citizen</h1>
+				<p class="text-2xl text-left mt-6">
+					Flatpak applications are a game-changer in the Linux world. They offer a streamlined way
+					to package and distribute software, ensuring compatibility across various Linux
+					distributions.
+				</p>
+				<p class="text-2xl text-left mt-6">
+					This means you no longer have to worry about dependency issues or whether an app will run
+					on your specific distro. Flatpaks are self-contained, bundling all the necessary libraries
+					and dependencies, making them easy to install and manage.
+				</p>
+				<p class="text-2xl text-left mt-6">
+					Aeon, recognizing the convenience and versatility of Flatpaks, wholeheartedly embraces
+					them as first-class citizens, offering seamless integration and support within its
+					ecosystem.
+				</p>
+			</article>
+		</div>
 
-		<Presentation
-			imageSrc="images/distrobox.png"
-			imageAlt="Distrobox"
-			heading="Distrobox"
-			text="Aeon takes customization to a new level with its support for Distrobox. This innovative
+		<div class="grid grid-cols-1 md:grid-cols-2 gap-8 items-center text-cente space-y-5">
+			<Presentation
+				imageSrc="images/distro_tmux.png"
+				imageAlt="Distrobox"
+				heading="Distrobox"
+				text="Aeon takes customization to a new level with its support for Distrobox. This innovative
 tool allows you to effortlessly run entire Linux distributions within your existing Aeon
 environment. Imagine having access to specific applications or tools from another distro
 without needing to switch systems entirely. Distrobox makes this a reality. Here's how
 Distrobox empowers Aeon users: Experimentation without Risk: Want to try a new distro or
 access software unavailable on Aeon?"
-		/>
-		<Presentation
-			imageSrc="images/microos.png"
-			imageAlt="MicroOS"
-			imageCaption=""
-			heading="MicroOS"
-			text="Aeon takes customization to a new level with its support for Distrobox. This innovative tool allows you to effortlessly run entire Linux distributions within your existing Aeon environment. Imagine having access to specific applications or tools from another distro without needing to switch systems entirely. Distrobox makes this a reality. Here's how Distrobox empowers Aeon users: Experimentation without Risk: Want to try a new distro or access software unavailable on Aeon?"
-		/>
+			/>
+			<article class="mx-5 md:p-32">
+				<h1 class="text-5xl text-primary-500">Distrobox</h1>
+				<h1 class="text-3xl text-primary-500 mt-3">for full flexibility</h1>
+				<p class="text-2xl text-left mt-6">
+					Distrobox is a revolutionary tool that takes customization to a new level by seamlessly
+					integrating with Aeon. It empowers users to effortlessly run entire Linux distributions
+					within their existing Aeon environment.
+				</p>
+				<p class="text-2xl text-left mt-6">
+					Experimentation without Risk: Want to try a new distro or access software unavailable on
+					Aeon? Distrobox lets you do so safely, without affecting your primary system.
+				</p>
+				<p class="text-2xl text-left mt-6">
+					Enhanced Workflow: Switch between different Distrobox containers effortlessly, maintaining
+					separate environments for different tasks or projects.
+				</p>
+			</article>
+		</div>
 	</div>
 </div>
-<hr class="mt-16" />
+<section class="bg-gradient-to-br from-gray-800 to-surface-800 py-8 md:px-24 mt-14">
+	<div class="container mx-auto justify-center mt-14">
+		<div class="space-y-10 text-left flex flex-col items-center">
+			<article class="text-wrap mx-5">
+				<h2 class="text-5xl text-center">
+					<span
+						class="bg-gradient-to-br from-primary-500 to-secondary-500 bg-clip-text text-transparent box-decoration-clone"
+						>Let Aeon handle it for you</span
+					>
+				</h2>
+				<p class="text-2xl mt-10">
+					Sit back, relax, and let Aeon take care of the technical details. Our static nature means
+					your Computer stays lightning-fast and secure, while automatic transactional updates
+					ensure your System is always fresh and up-to-date.
+				</p>
+				<div class="py-12 md:px-16 text-2xl">
+					<ul class="list-disc space-y-4 pl-2 md:pl-12">
+						<li class="flex items-center">
+							<img src="images/checkmark.svg" alt="Checkmark" class="w-12 h-12 md:w-8 md:h-8 text-green-500 mr-2" />
+							<span>Aeon leverages transactional updates and btrfs snapshots for atomic, risk-free system updates.</span>
+						</li>
+				
+						<li class="flex items-center ">
+							<img src="images/checkmark.svg" alt="Checkmark" class="w-12 h-12 md:w-8 md:h-8 text-green-500 mr-2" />
+							<span>In the event of a failed update, the system seamlessly rolls back to the previous snapshot, ensuring your system remains bootable and functional.</span>
+						</li>
+				
+						<li class="flex items-center ">
+							<img src="images/checkmark.svg" alt="Checkmark" class="w-12 h-12 md:w-8 md:h-8 text-green-500 mr-2" />
+							<span>Snapshots include your configuration in /etc, empowering you to easily revert configuration changes if needed.</span>
+						</li>
+				
+						<li class="flex items-center">
+							<img src="images/checkmark.svg" alt="Checkmark" class="w-12 h-12 md:w-8 md:h-8 text-green-500 mr-2" />
+							<span>Transactional updates support both your existing RPM packages and most packages in Aeon's repositories.</span>
+						</li>
+					</ul>
+				</div>
+			</article>
+		</div>
+	</div>
+</section>
+
 <div class="container mx-auto justify-center items-center mt-16">
-	<h2 class="h2 text-center">
+	<h2 class="text-5xl text-center">
 		<span
 			class="bg-gradient-to-br from-primary-500 to-secondary-400 bg-clip-text text-transparent box-decoration-clone mx-5"
 			>Further Information</span
@@ -177,8 +271,8 @@ access software unavailable on Aeon?"
 					>
 						<img class="object-cover h-full w-full" src="images/wiki.svg" alt="" />
 					</div>
-					<h3 class=" text-xl font-bold mt-4">Technical Details</h3>
-					<p class=" text-base mt-2 text-balance text-center">
+					<h3 class=" text-4xl font-bold mt-4">Technical Details</h3>
+					<p class=" text-2xl mt-2 text-balance text-center">
 						For technical details and how-tos, head over to the Wiki
 					</p>
 					<button
@@ -194,8 +288,8 @@ access software unavailable on Aeon?"
 					>
 						<img class="object-cover h-full w-full" src="images/video.svg" alt="" />
 					</div>
-					<h3 class=" text-xl font-bold mt-4">Project Origins</h3>
-					<p class=" text-base mt-2 text-balance text-center">
+					<h3 class=" text-4xl font-bold mt-4">Project Origins</h3>
+					<p class=" text-2xl mt-2 text-balance text-center">
 						For a deep dive on the origins and use-cases for Aeon, check out this video:
 					</p>
 					<button
