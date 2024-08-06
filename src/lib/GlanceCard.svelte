fccde4e1d84f28a37dcd5084f21c505b1240a9a4
Marius Biegger <biegger.marius@gmail.com>
2024-08-06 16:33:28 +0200

    added github logo

--- src/lib/GlanceCard.svelte
+++ src/lib/GlanceCard.svelte
@@ -8,7 +8,7 @@
   
   <div class="card hover:shadow-lg mx-2 card-hover">
     <header class="card-header flex flex-col items-center p-4">
-      <div class="h-14 w-14 overflow-hidden transform transition duration-300 ease-in-out hover:rotate-10">
+      <div class="h-14 w-14">
         <img class="object-cover h-full w-auto" src="{image}" alt="{alt}" />
       </div>
       <h3 class="text-2xl font-bold mt-4">{header}</h3>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+
 </script>
 
 <!-- App Shell -->
@@ -9,10 +10,14 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<img class="w-6 h-6" src="images/logo.svg" alt="Aeon Logo">
+				<img class="w-8 h-8" src="images/logo.svg" alt="Aeon Logo">
 				<strong class="text-xl uppercase ml-2">Aeon</strong>
 			</svelte:fragment>
-			
+			<svelte:fragment slot="trail">
+				<a href="https://github.com/bieggerm/web-aeon" target="_blank" rel="noopener noreferrer">
+						<img class="w-8 h-8"src="images/github-logo.svg" alt="github-logo">
+				</a>
+			</svelte:fragment>
 		</AppBar>
 		
 	</svelte:fragment>
